11e71280f05d335c9cb741c4034b7b0cd1e395b7
ITAQ <it@alphaquimicasrl.com.ar>
2026-02-24 14:23:50 -0300

    FORZANDO URL CON GUION S-O

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Agregamos este log para verlo en la consola del navegador
+console.log("--- CHEQUEO DE URL ---");
+console.log("La URL configurada es: https://backend-s-o.onrender.com");
+
 const api = axios.create({
-  // USA LA URL QUE SALE EN EL ÚLTIMO LOG DE RENDER
   baseURL: 'https://backend-s-o.onrender.com', 
 });
 
