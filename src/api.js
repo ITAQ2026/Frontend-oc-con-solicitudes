0cc881ac455e4b35359de68b027ac3511f72777a
ITAQ <it@alphaquimicasrl.com.ar>
2026-02-24 12:51:52 -0300

    Confirmando URL de producción en Render

--- src/api.js
+++ src/api.js
@@ -1,16 +1,6 @@
-//import axios from 'axios';
-
-//const api = axios.create({
-//  baseURL: 'http://localhost:3000', // Donde vive NestJS
-//});
-
-//export default api;
-
-
 import axios from 'axios';
 
 const api = axios.create({
-  // Cambia el localhost por tu URL de Render
   baseURL: 'https://backend-s-o.onrender.com', 
 });
 
