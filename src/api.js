8aba39fbf1f47f378c6446e5c3fd053cf2b82e3f
ITAQ <it@alphaquimicasrl.com.ar>
2026-02-24 11:59:42 -0300

    Actualizando URL de API en el frontend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,17 @@
+//import axios from 'axios';
+
+//const api = axios.create({
+//  baseURL: 'http://localhost:3000', // Donde vive NestJS
+//});
+
+//export default api;
+
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Donde vive NestJS
+  // Cambia el localhost por tu URL de Render
+  baseURL: 'https://backend-s-o.onrender.com', 
 });
 
 export default api;
